5d65c1a5c746b9d17f4b361ffb88723605e53c33
Nico Gallinal <ngallinal@cocos.capital>
2025-10-27 09:55:57 -0300

    feat: type _getJSONData method

--- test/lib/http-mock.test-d.ts
+++ test/lib/http-mock.test-d.ts
@@ -26,6 +26,14 @@ expectNotType<MockResponse<ExpressResponse>>(createResponse<NodeResponse>());
 expectAssignable<NodeResponse>(createResponse<NodeResponse>());
 expectNotAssignable<ExpressResponse>(createResponse<NodeResponse>());
 
+expectType<MockResponse<ExpressResponse<{ message: number }>>>(createResponse<ExpressResponse<{ message: number }>>());
+expectNotType<MockResponse<NodeResponse>>(createResponse<ExpressResponse<{ message: number }>>());
+expectAssignable<NodeResponse>(createResponse<ExpressResponse<{ message: number }>>());
+
 expectType<Mocks<ExpressRequest, ExpressResponse>>(createMocks());
 // eslint-disable-next-line no-undef
 expectType<Mocks<globalThis.Request, globalThis.Response>>(createMocks<globalThis.Request, globalThis.Response>());
+
+expectType<Mocks<ExpressRequest, ExpressResponse<{ message: number }>>>(
+    createMocks<ExpressRequest, ExpressResponse<{ message: number }>>()
+);
